d300a45808c284c38e7a8c41d15e9b67e455f3ec
krishc0de <41341381+krishc0de@users.noreply.github.com>
2025-05-10 05:36:43 +0200

    Create config.js

---
+++ docs /.vitepress/config.js
@@ -0,0 +1,12 @@
+export default {
+  title: 'My Site',
+  base: '/rootfx-io.github.io/',  // Important for GitHub Pages!
+  themeConfig: {
+    nav: [
+      { text: 'Book', link: '/book' },
+      { text: 'About Me', link: '/about' },
+      { text: 'Software', link: '/software' },
+      { text: 'Blog', link: '/blog/' }
+    ],
+  },
+}
