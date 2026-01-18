8c82510ad02a8ded89efa7604d7d2ef09623d231
krishc0de <41341381+krishc0de@users.noreply.github.com>
2026-01-18 13:59:08 +0100

    Update config.js

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -20,10 +20,10 @@ export default {
   ],
   themeConfig: {
     nav: [
-      { text: 'Book', link: '/book/' },
-      { text: 'About Me', link: '/about/' },
-      { text: 'Software', link: '/software/' },
-      { text: 'Blog', link: '/blog/' }
+      { text: 'Book', link: '/book' },
+      { text: 'About Me', link: '/about' },
+      { text: 'Software', link: '/software' },
+      { text: 'Blog', link: '/blog' }
     ],
   }
 }
