4919914cdc1af4bb8fad86cfedb25833b0ceab76
krishc0de <41341381+krishc0de@users.noreply.github.com>
2026-01-18 13:38:35 +0100

    Update config.js

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,5 +1,22 @@
 export default {
   title: "Rootfx.io",
+  
+ head: [
+    [
+      'script',
+      { async: '', src: 'https://www.googletagmanager.com/gtag/js?id=G-4Q7YP9FWLN' }
+    ],
+    [
+      'script',
+      {},
+      `
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', 'G-4Q7YP9FWLN');
+      `
+    ]
+  ],
   themeConfig: {
     nav: [
       { text: 'Book', link: '/book' },
