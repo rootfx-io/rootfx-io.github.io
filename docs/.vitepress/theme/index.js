08c6438f5a4eef0a28b23184f1710ab182a36936
krishc0de <41341381+krishc0de@users.noreply.github.com>
2026-01-18 14:09:51 +0100

    Update index.js

--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -1,5 +1,27 @@
 import DefaultTheme from 'vitepress/theme'
 
 export default {
-  extends: DefaultTheme
+  extends: DefaultTheme,
+
+  enhanceApp({ router }) {
+    if (typeof window === 'undefined') return
+
+    const script = document.createElement('script')
+    script.async = true
+    script.src = 'https://www.googletagmanager.com/gtag/js?id=G-4Q7YP9FWLN'
+    document.head.appendChild(script)
+
+    window.dataLayer = window.dataLayer || []
+    function gtag(){window.dataLayer.push(arguments)}
+    window.gtag = gtag
+
+    gtag('js', new Date())
+    gtag('config', 'G-4Q7YP9FWLN', { anonymize_ip: true })
+
+    router.afterEach((to) => {
+      window.gtag('config', 'G-4Q7YP9FWLN', {
+        page_path: to.path
+      })
+    })
+  }
 }
